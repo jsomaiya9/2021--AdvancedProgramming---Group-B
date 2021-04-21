33e224c0b5db9d7ebde81a57ca0a53f8b7aba03a
Bilkis Islam <365405@my.leicestercollege.ac.uk>
2021-04-21 11:03:03 +0100

    Manual database

---
+++ BARDSports/BARDSports/Models/BARDSportsDataInitialiser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BARDSports.Models
+{
+    public class BARDSportsDataInitialiser: DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            LeagueModel leagueModel1 = new LeagueModel();
+            leagueModel1.LeagueId = 1;
+            leagueModel1.LeagueName = "Premier League";
+            context.LeagueModels.Add(leagueModel1);
+
+
+        }
+}
